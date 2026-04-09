bac933a56359580e1352205c3fc0c7f9644c6278
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-09 17:29:15 -0500

    feat: implement Prisma module and service with database connection lifecycle management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
+// @Global()
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
