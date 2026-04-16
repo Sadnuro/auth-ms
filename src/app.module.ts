bc8406300bf661520179a1fef2d73811491f0cd2
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-16 15:20:53 -0500

    feat: integrate Redis caching into AppModule and update environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis from '@keyv/redis';
+import { envs } from 'src/config';
 
 // @Global()
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    // Redis Cache Config
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => {
+        return {
+          ttl: 5000,
+          stores: [new KeyvRedis(envs.REDIS_URL)],
+        };
+      },
+    }),
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import { z } from 'zod';
 
-export const envSchema = z.object({
+export const envSchema = z
+  .object({
     PORT: z.coerce.number().min(1, 'AUTH_MS_PORT is required.'),
     HOST: z.string(),
     // DB_HOST: z.string(),
@@ -9,26 +10,32 @@ export const envSchema = z.object({
     // DB_NAME: z.string(),
     // DB_USER: z.string(),
     // DB_PASSWORD: z.string(),
-    DATABASE_URL: z.string(),
-    ALLOWED_ORIGINS: z.string().transform((val) => val.split(',').map((origin) => origin.trim())),
-}).passthrough();
+    DATABASE_URL: z.string().min(1, 'DATABASE_URL is required.'),
+    REDIS_URL: z.string().min(1, 'REDIS_URL is required.'),
+
+    ALLOWED_ORIGINS: z
+      .string()
+      .transform((val) => val.split(',').map((origin) => origin.trim())),
+  })
+  .passthrough();
 
 type EnvType = z.infer<typeof envSchema>;
 const envParsed = envSchema.safeParse(process.env);
 
 if (!envParsed.success) {
-    console.log('Environment Validation Error:', envParsed.error.format());
-    throw new Error('Environment Validation Error');
+  console.log('Environment Validation Error:', envParsed.error.format());
+  throw new Error('Environment Validation Error');
 }
 
 export const envs: EnvType = {
-    PORT: envParsed.data.PORT,
-    HOST: envParsed.data.HOST,
-    ALLOWED_ORIGINS: envParsed.data.ALLOWED_ORIGINS,
-    // DB_HOST: envParsed.data.DB_HOST,
-    // DB_PORT: envParsed.data.DB_PORT,
-    // DB_NAME: envParsed.data.DB_NAME,
-    // DB_USER: envParsed.data.DB_USER,
-    // DB_PASSWORD: envParsed.data.DB_PASSWORD,
-    DATABASE_URL: envParsed.data.DATABASE_URL,
+  PORT: envParsed.data.PORT,
+  HOST: envParsed.data.HOST,
+  ALLOWED_ORIGINS: envParsed.data.ALLOWED_ORIGINS,
+  // DB_HOST: envParsed.data.DB_HOST,
+  // DB_PORT: envParsed.data.DB_PORT,
+  // DB_NAME: envParsed.data.DB_NAME,
+  // DB_USER: envParsed.data.DB_USER,
+  // DB_PASSWORD: envParsed.data.DB_PASSWORD,
+  DATABASE_URL: envParsed.data.DATABASE_URL,
+  REDIS_URL: envParsed.data.REDIS_URL,
 };
