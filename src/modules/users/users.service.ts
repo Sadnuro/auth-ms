783b437c87ed68f7b9ab03f2ca8a921d13c59aff
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-16 18:50:40 -0500

    feat: add email uniqueness validation to user creation service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { CreateUserInterface } from 'src/modules/users/interfaces/user.interface';
 
@@ -6,7 +6,18 @@ import { CreateUserInterface } from 'src/modules/users/interfaces/user.interface
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
+  private async validateEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      select: { email: true },
+    });
+    if (user) {
+      throw new BadRequestException('Email already exists');
+    }
+  }
+
   async create(user: CreateUserInterface) {
+    await this.validateEmail(user.email);
     return await this.prisma.user.create({
       data: user,
     });
