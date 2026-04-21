86350c2242e434cf01b2f4039416d82fa2b3deb3
Sadith Nuñez <sadnr0213@gmail.com>
2026-04-20 20:10:52 -0500

    feat: add bcrypt comparison endpoint to test controller and import BcryptModule

--- src/modules/test/test.controller.ts
+++ src/modules/test/test.controller.ts
@@ -1,13 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { BcryptService } from 'src/modules/bcrypt/bcrypt.service';
 import { UsersService } from 'src/modules/users/users.service';
 
 @Controller('test')
 export class TestController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly bcrypt: BcryptService,
+  ) {}
 
   @Post()
   async test(@Body() data: any) {
     console.log(data);
     return this.usersService.create(data);
+  }
+  @Post('/bcrypt-compare')
+  async compare(@Body() data: any) {
+    const password = data.password;
+    const hash = data.hash;
+    console.log(data);
+
+    return this.bcrypt.compare(password, hash);
   }
 }
--- src/modules/test/test.module.ts
+++ src/modules/test/test.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TestService } from './test.service';
 import { TestController } from 'src/modules/test/test.controller';
 import { UsersModule } from 'src/modules/users/users.module';
+import { BcryptModule } from 'src/modules/bcrypt/bcrypt.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BcryptModule],
   controllers: [TestController],
   providers: [TestService],
   exports: [TestService],
